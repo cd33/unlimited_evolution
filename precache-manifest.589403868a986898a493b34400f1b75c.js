b1107df5e59bf5ea18dc09c81452dd34b0b406fa
cd33 <charles.diallo@ynov.com>
2022-01-13 17:20:40 +0100

    Updates

---
+++ precache-manifest.589403868a986898a493b34400f1b75c.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b428801f09967109d740af326d738d64",
+    "url": "/UnlimitedEvolution/index.html"
+  },
+  {
+    "revision": "1a3bee59a610316c76c3",
+    "url": "/UnlimitedEvolution/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "11876c2368372b927356",
+    "url": "/UnlimitedEvolution/static/css/main.559e94e9.chunk.css"
+  },
+  {
+    "revision": "1a3bee59a610316c76c3",
+    "url": "/UnlimitedEvolution/static/js/2.43c70871.chunk.js"
+  },
+  {
+    "revision": "11876c2368372b927356",
+    "url": "/UnlimitedEvolution/static/js/main.54f8a97f.chunk.js"
+  },
+  {
+    "revision": "25f5003036c95d808252",
+    "url": "/UnlimitedEvolution/static/js/runtime-main.a3b2b75d.js"
+  },
+  {
+    "revision": "893b68bf58d4c4d22968b5a27c72f04b",
+    "url": "/UnlimitedEvolution/static/media/Arena.893b68bf.jpg"
+  },
+  {
+    "revision": "5f8988cd5d80b616fc29a89b1d0e5b2d",
+    "url": "/UnlimitedEvolution/static/media/MyCharacters.5f8988cd.jpg"
+  },
+  {
+    "revision": "e2adb7fc8a8c052dfd004b60ba2c8127",
+    "url": "/UnlimitedEvolution/static/media/MyStuff.e2adb7fc.jpg"
+  },
+  {
+    "revision": "8fdd3dc387c83a602cd017ffeee7e3d0",
+    "url": "/UnlimitedEvolution/static/media/black.8fdd3dc3.png"
+  },
+  {
+    "revision": "9e26e2057f774f68f35c0b1375811bf8",
+    "url": "/UnlimitedEvolution/static/media/black.9e26e205.png"
+  },
+  {
+    "revision": "3e578e65c6668812d3c8e4d8b4659124",
+    "url": "/UnlimitedEvolution/static/media/blackWhite.3e578e65.png"
+  },
+  {
+    "revision": "31e48bf6d57c2918c025968c6674ee32",
+    "url": "/UnlimitedEvolution/static/media/brown.31e48bf6.png"
+  },
+  {
+    "revision": "f5d05dd9921330e355e8d8c308f91541",
+    "url": "/UnlimitedEvolution/static/media/forest.f5d05dd9.png"
+  },
+  {
+    "revision": "f45f95de9afe9d63aa3eda9cfb380549",
+    "url": "/UnlimitedEvolution/static/media/grass.f45f95de.png"
+  },
+  {
+    "revision": "c17194f5ae34481ec8d575fc2b6d9237",
+    "url": "/UnlimitedEvolution/static/media/green.c17194f5.png"
+  },
+  {
+    "revision": "2d764394e04452d209244d00c5768284",
+    "url": "/UnlimitedEvolution/static/media/home.2d764394.jpg"
+  },
+  {
+    "revision": "c405a7669187914465cebc1aeada4525",
+    "url": "/UnlimitedEvolution/static/media/logo.c405a766.png"
+  },
+  {
+    "revision": "26c503e9e1f3ed59722c0a30735bc391",
+    "url": "/UnlimitedEvolution/static/media/ocean.26c503e9.png"
+  },
+  {
+    "revision": "4cfc342f3d8ff6f100d89a416c60520d",
+    "url": "/UnlimitedEvolution/static/media/potion.4cfc342f.png"
+  },
+  {
+    "revision": "21f4e36f97917d64755a8fe0b06d05e3",
+    "url": "/UnlimitedEvolution/static/media/punk.21f4e36f.png"
+  },
+  {
+    "revision": "a202bb4f55d10655ecc1c39fa2383f65",
+    "url": "/UnlimitedEvolution/static/media/purple.a202bb4f.png"
+  },
+  {
+    "revision": "8515c3dbf70eda708b63d2e3589fe324",
+    "url": "/UnlimitedEvolution/static/media/rainbow.8515c3db.png"
+  },
+  {
+    "revision": "183424b3294a94a38b6d531f9ab9853f",
+    "url": "/UnlimitedEvolution/static/media/red.183424b3.png"
+  },
+  {
+    "revision": "60d4cfffe6f8f0bc170688941f809555",
+    "url": "/UnlimitedEvolution/static/media/shieldAegis.60d4cfff.png"
+  },
+  {
+    "revision": "9a5bf9bd48f2ac1e7e15e96adac6ef3f",
+    "url": "/UnlimitedEvolution/static/media/shieldAegis.9a5bf9bd.png"
+  },
+  {
+    "revision": "881358ef09536458cb689d0022a81c23",
+    "url": "/UnlimitedEvolution/static/media/shieldBasic.881358ef.png"
+  },
+  {
+    "revision": "95381f390dcfeee305e7594bd9fe3959",
+    "url": "/UnlimitedEvolution/static/media/shieldBasic.95381f39.png"
+  },
+  {
+    "revision": "f38b2e72a0031b3e2c229cbfe42ba63d",
+    "url": "/UnlimitedEvolution/static/media/sun.f38b2e72.png"
+  },
+  {
+    "revision": "2b11b4c0765fc492996f231c1e794dc7",
+    "url": "/UnlimitedEvolution/static/media/swordBasic.2b11b4c0.png"
+  },
+  {
+    "revision": "7b20d00387d2ecd1c6e1905bc80e7877",
+    "url": "/UnlimitedEvolution/static/media/swordBasic.7b20d003.png"
+  },
+  {
+    "revision": "84ec7fa0234f2136a4f03e0881974313",
+    "url": "/UnlimitedEvolution/static/media/swordExcalibur.84ec7fa0.png"
+  },
+  {
+    "revision": "9070b76518588b7575e31d9dcaa7cd9b",
+    "url": "/UnlimitedEvolution/static/media/swordExcalibur.9070b765.png"
+  },
+  {
+    "revision": "a31f59f4842de9d4bc856396f41aaebc",
+    "url": "/UnlimitedEvolution/static/media/ugly.a31f59f4.png"
+  },
+  {
+    "revision": "a666de7732bf1faf893dd235ed3345d1",
+    "url": "/UnlimitedEvolution/static/media/volcano.a666de77.png"
+  },
+  {
+    "revision": "35cf5d4923cee516b127733095f507b2",
+    "url": "/UnlimitedEvolution/static/media/white.35cf5d49.png"
+  },
+  {
+    "revision": "6e91c150a884ab39aecddfb4ce1cd371",
+    "url": "/UnlimitedEvolution/static/media/white.6e91c150.png"
+  }
+]);

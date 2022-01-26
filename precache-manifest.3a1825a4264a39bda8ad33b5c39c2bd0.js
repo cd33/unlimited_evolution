402e7cc8248c66a7ef1ee8f59b3066d4b760ced4
cd33 <charles.diallo@ynov.com>
2022-01-26 17:03:27 +0100

    Updates

--- precache-manifest.3a1825a4264a39bda8ad33b5c39c2bd0.js
+++ precache-manifest.3a1825a4264a39bda8ad33b5c39c2bd0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38ca2121478ac9959a077bb3eccc1b7b",
+    "revision": "63ce8a09c6e3d88432dcbb03281f16b4",
     "url": "/unlimited_evolution/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/unlimited_evolution/static/css/2.68a81a89.chunk.css"
   },
   {
-    "revision": "262683bbf1a6a0342b15",
+    "revision": "c5ab3f545409a5cf23c7",
     "url": "/unlimited_evolution/static/css/main.f1a9e059.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/unlimited_evolution/static/js/2.aa60c179.chunk.js"
   },
   {
-    "revision": "262683bbf1a6a0342b15",
-    "url": "/unlimited_evolution/static/js/main.2252758c.chunk.js"
+    "revision": "c5ab3f545409a5cf23c7",
+    "url": "/unlimited_evolution/static/js/main.4cc2c42f.chunk.js"
   },
   {
     "revision": "850c4e3d965a02ed501b",
